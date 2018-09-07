72621f4a8c98437f93deb2545bd0cc3467777c34
phachon <phachon@163.com>
2018-09-07 16:21:16 +0800

    end search

--- Search/code/php/Search.php
+++ Search/code/php/Search.php
@@ -11,6 +11,22 @@ class Search {
 	}
 
 	/**
+	 * 顺序查找
+	 * @param array $data
+	 * @param $key
+	 * @return int
+	 */
+	public function sequence(array $data, $key) {
+		$count = count($data);
+		for ($i = 0; $i < $count; $i++) {
+			if ($data[$i] == $key) {
+				return $i;
+			}
+		}
+		return -1;
+	}
+
+	/**
 	 * 二分查找
 	 * @param array $data 待查找的数组
 	 * @param int $key 查找的数据
@@ -30,6 +46,79 @@ class Search {
 				return $middle;
 			}
 		}
+		return -1;
+	}
+
+	/**
+	 * 插值查找
+	 * @param array $data
+	 * @param $key
+	 * @return float|int
+	 */
+	public function interpolation(array $data, $key) {
+		$start = 0;
+		$end = count($data) - 1;
+
+		while ($start < $end) {
+			$middle =  $start + ($end - $start) * intval(($key - $data[$start]) / ($data[$end] - $data[$start]));
+			if ($key > $data[$middle]) {
+				$start = $middle + 1;
+			}else if ($key < $data[$middle]) {
+				$end = $middle - 1;
+			}else {
+				return $middle;
+			}
+		}
+		return -1;
+	}
+
+	/**
+	 * 斐波那契查找
+	 * @param array $data
+	 * @param $key
+	 * @return int|mixed
+	 */
+	public function fibonacci(array $data, $key) {
+		$start = 0;
+		$count = count($data);
+		$end = $count - 1;
+
+		// 构建斐波那契数组
+		$fibo = [1, 1];
+		for ($i = 2; $i < 20; $i++) {
+			$fibo[$i] = $fibo[$i - 1]+$fibo[$i - 2];
+		}
+
+		// 查找 count 最接近的斐波那契数的位置
+		for ($k = 0; $k < count($fibo); $k++) {
+			if ($fibo[$k] > $count) {
+				break;
+			}
+		}
+
+		// 扩充数据, fibo[key] - 1 的长度
+		for ($i = $count; $i < $fibo[$k] - 1; $i++) {
+			$data[$i] = $data[$end];
+		}
+
+		// 比较数据
+		while ($start <= $end) {
+			$middle = $start + $fibo[$k - 1] - 1;
+			if ($key > $data[$middle]) {
+				$start = $middle + 1;
+				$k -= 2;
+			}else if($key < $data[$middle]) {
+				$end = $middle - 1;
+				$k -= 1;
+			}else {
+				if ($middle < $count) {
+					return $middle;
+				}else {
+					return $count - 1;
+				}
+			}
+		}
+
 		return -1;
 	}
 }
--- Search/code/php/Search_Test.php
+++ Search/code/php/Search_Test.php
@@ -5,13 +5,36 @@
 
 include 'Search.php';
 
-$data = [11, 4, 17, 889, 26, 28, 3, 72];
-
 $sort = new Search();
 
+// 顺序查找
+echo "Sequence: ";
+$data = [11, 4, 17, 889, 26, 28, 3, 72];
+echo $sort->sequence($data, 4)."|";
+echo $sort->sequence($data, 10)."|";
+echo $sort->sequence($data, 3)."|";
+echo "\r\n";
+
 // 二分查找
-echo $sort->binary($data, 4)."\r\n";
-echo $sort->binary($data, 10)."\r\n";
+$data = [1, 4, 7, 9, 26, 28, 33, 72];
+echo "Binary: ";
+echo $sort->binary($data, 4)."|";
+echo $sort->binary($data, 10)."|";
+echo $sort->binary($data, 33)."|";
+echo "\r\n";
 
+// 插值查找
+echo "Interpolation: ";
+$data = [1, 4, 7, 9, 26, 28, 33, 72];
+echo $sort->interpolation($data, 4)."|";
+echo $sort->interpolation($data, 19)."|";
+echo $sort->interpolation($data, 72)."|";
+echo "\r\n";
 
-
+// 斐波那契查找
+echo "Fibonacci: ";
+$data = [1, 4, 7, 9, 26, 28, 33, 72];
+echo $sort->fibonacci($data, 33)."|";
+echo $sort->fibonacci($data, 19)."|";
+echo $sort->fibonacci($data, 72)."|";
+echo "\r\n";
