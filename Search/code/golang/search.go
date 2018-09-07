0a00b33e82b7b106f07969f1b51a4803b24e726f
phachon <phachon@163.com>
2018-09-07 16:51:33 +0800

    search go

--- Search/code/golang/search.go
+++ Search/code/golang/search.go
@@ -4,11 +4,24 @@ type Search struct {
 
 }
 
+
+// 顺序查找
+// data: 待查找的数组
+// key: 待查找的 key
+func (s *Search) Sequence(data []int, key int) int {
+	count := len(data)
+	for i := 0; i < count; i ++ {
+		if data[i] == key {
+			return i
+		}
+	}
+	return -1
+}
+
 // 二分查找
 // data: 待查找的数组
 // key: 待查找的 key
 func (s *Search) Binary(data []int, key int) int {
-
 	start := 0
 	end := len(data)
 	for start <= end {
@@ -19,6 +32,81 @@ func (s *Search) Binary(data []int, key int) int {
 			end = middle - 1
 		}else {
 			return middle
+		}
+	}
+	return -1
+}
+
+// 插值查找
+// data: 待查找的数组
+// key: 待查找的 key
+func (s *Search) Interpolation(data []int, key int) int {
+	start := 0
+	end := len(data) - 1
+
+	for start <= end {
+		middle := start + (end - start) * (data[key] - data[start]) / (data[end] - data[start])
+		if key > data[middle] {
+			start = middle + 1
+		}else if key < data[middle] {
+			end = middle - 1
+		}else {
+			return middle
+		}
+ 	}
+
+ 	return -1
+}
+
+// 斐波那契查找
+// data: 待查找的数组
+// key: 待查找的 key
+func (s *Search) Fibonacci(data []int, key int) int {
+	start := 0
+	count := len(data)
+	end := count - 1
+
+	// 构造一个斐波那契数组
+	f := make([]int, 20)
+	f[0] = 1
+	f[1] = 1
+	for i := 2; i < 20; i++ {
+		f[i] = f[i - 1] + f[i -2]
+	}
+
+	// 查找 count 最接近的斐波那契数的位置
+	var k int
+	for k = 0; k < 20; k++ {
+		if f[k] > count {
+			break
+		}
+	}
+
+	tmp := make([]int, f[k] - 1)
+	// 补充数据，满足 f[k] - 1 = count
+	for i := 0; i < f[k] - 1; i++ {
+		if i < count {
+			tmp[i] = data[i]
+		}else {
+			tmp[i] = data[end]
+		}
+	}
+
+	// 循环比较
+	for start <= end {
+		middle := start + f[k -1] - 1
+		if key > data[middle] {
+			start = middle + 1
+			k -= 2
+		}else if key < data[middle] {
+			end = middle - 1
+			k -= 1
+		}else {
+			if start < count {
+				return middle
+			}else {
+				return end
+			}
 		}
 	}
 	return -1
--- Search/code/golang/searchTest.go
+++ Search/code/golang/searchTest.go
@@ -6,11 +6,39 @@ import "fmt"
 func main() {
 
 	search := &Search{}
+
+
+	// 顺序查找
 	data := []int{11, 4, 17, 889, 26, 28, 3, 72}
+	fmt.Printf("%s", "Sequence: ")
+	fmt.Printf("%d|", search.Sequence(data, 4))
+	fmt.Printf("%d|", search.Sequence(data, 10))
+	fmt.Printf("%d|", search.Sequence(data, 3))
+	fmt.Println()
 
 	// 二分查找
-	fmt.Println(search.Binary(data, 4))
-	fmt.Println(search.Binary(data, 10))
+	data = []int{1, 4, 7, 9, 26, 28, 33, 72}
+	fmt.Printf("%s", "Binary: ")
+	fmt.Printf("%d|", search.Binary(data, 4))
+	fmt.Printf("%d|", search.Binary(data, 10))
+	fmt.Printf("%d|", search.Binary(data, 33))
+	fmt.Println()
+
+	// 插值查找
+	data = []int{1, 4, 7, 9, 26, 28, 33, 72}
+	fmt.Printf("%s", "Interpolation: ")
+	fmt.Printf("%d|", search.Binary(data, 4))
+	fmt.Printf("%d|", search.Binary(data, 10))
+	fmt.Printf("%d|", search.Binary(data, 33))
+	fmt.Println()
+
+	// 斐波那契查找
+	data = []int{1, 4, 7, 9, 26, 28, 33, 72}
+	fmt.Printf("%s", "Fibonacci: ")
+	fmt.Printf("%d|", search.Fibonacci(data, 4))
+	fmt.Printf("%d|", search.Fibonacci(data, 10))
+	fmt.Printf("%d|", search.Fibonacci(data, 33))
+	fmt.Println()
 }
 
 
